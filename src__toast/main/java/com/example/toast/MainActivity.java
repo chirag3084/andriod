0d95b07918092a97e3d8bc97d11f4eaa0b0705dc
CODY <110027137+chirag3084@users.noreply.github.com>
2025-04-02 12:56:55 +0530

    Add files via upload

---
+++ src__toast/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.toast;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Good Morning", Toast.LENGTH_SHORT).show();
+    }
+}
